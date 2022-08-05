ed818a2b1f5a30fa0ee9bd845dd6c27773426f14
lile <285051173@qq.com>
2022-08-05 15:07:24 +0800

    服务器少开端口

--- src/test/java/io/le/HttpProxyServerStartupTest.java
+++ src/test/java/io/le/HttpProxyServerStartupTest.java
@@ -8,7 +8,7 @@ import io.le.http.proxy.server.config.HttpProxyServerConfig;
 
 import java.util.Arrays;
 
-public class HttpProxyServerStartup {
+public class HttpProxyServerStartupTest {
     public static void main(String[] args) {
         // demo6789();
 
@@ -21,6 +21,31 @@ public class HttpProxyServerStartup {
         httpProxyServerConfig.setBossGroupThreads(5);
         httpProxyServerConfig.setWorkerGroupThreads(10);
         httpProxyServer.start(httpProxyServerConfig);
+
+        // 中继代理
+        HttpProxyRelayServer httpRelayProxyServer = new HttpProxyRelayServer();
+        HttpProxyRelayServerConfig httpProxyRelayServerConfig = new HttpProxyRelayServerConfig();
+        httpProxyRelayServerConfig.setCodecSsl(false);
+        httpProxyRelayServerConfig.setProxyProtocol(HttpProxyServerConfig.ProxyProtocol.HTTP);
+        httpProxyRelayServerConfig.setRelayProtocol(HttpProxyServerConfig.ProxyProtocol.LEE);
+        // httpProxyRelayServerConfig.setRealProxyHost("74.91.26.90");
+        httpProxyRelayServerConfig.setRealProxyHost("8.210.18.42");
+        httpProxyRelayServerConfig.setRealProxyPort(40000);
+        // httpProxyRelayServerConfig.setProxyHost("sprint.ikeatw.com");
+        // httpProxyRelayServerConfig.setProxyPort(50000);
+        // httpProxyRelayServerConfig.setProxyUsername("sprint");
+        // httpProxyRelayServerConfig.setProxyPassword("FtMM7EvG");
+
+        httpProxyRelayServerConfig.setPort(40001);
+        httpProxyRelayServerConfig.setBossGroupThreads(5);
+        httpProxyRelayServerConfig.setWorkerGroupThreads(10);
+
+        ReplayRuleConfig replayRuleConfig = new ReplayRuleConfig();
+        replayRuleConfig.setDirectHosts(Arrays.asList("ip111", "baidu.com"));
+        httpProxyRelayServerConfig.setReplayFilterConfig(replayRuleConfig);
+        httpRelayProxyServer.start(httpProxyRelayServerConfig);
+
+        // curl -x 127.0.0.1:8888 --insecure https://www.baidu.com
     }
 
     private static void demo6789() {
