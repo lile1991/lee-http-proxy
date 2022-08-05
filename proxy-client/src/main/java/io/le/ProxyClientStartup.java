700ac57d769a5ba7688a3f8ce234cc243efd2555
lile <285051173@qq.com>
2022-08-05 16:09:59 +0800

    proxy client

--- proxy-client/src/main/java/io/le/ProxyClientStartup.java
+++ proxy-client/src/main/java/io/le/ProxyClientStartup.java
@@ -1,7 +1,54 @@
 package io.le;
 
+import io.le.proxy.client.gui.TrayGUI;
+import io.le.proxy.server.relay.HttpProxyRelayServer;
+import io.le.proxy.server.relay.config.HttpProxyRelayServerConfig;
+import io.le.proxy.server.relay.config.ReplayRuleConfig;
+import io.le.proxy.server.server.config.HttpProxyServerConfig;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.swing.*;
+import java.util.Arrays;
+
+@Slf4j
 public class ProxyClientStartup {
     public static void main(String[] args) {
+        System.setProperty("java.awt.headless", "false");
 
+        try {
+            org.jb2011.lnf.beautyeye.BeautyEyeLNFHelper.launchBeautyEyeLNF();
+            // 不显示设置按钮
+            UIManager.put("RootPane.setupButtonVisible", false);
+        } catch(Exception e) {
+            log.warn("Apply skin Exception: " + e.getMessage());
+        }
+
+        TrayGUI trayGUI = new TrayGUI();
+        proxySystem();
+    }
+
+    private static void proxySystem() {
+        // 中继代理
+        HttpProxyRelayServer httpRelayProxyServer = new HttpProxyRelayServer();
+        HttpProxyRelayServerConfig httpProxyRelayServerConfig = new HttpProxyRelayServerConfig();
+        httpProxyRelayServerConfig.setCodecSsl(false);
+        httpProxyRelayServerConfig.setProxyProtocol(HttpProxyServerConfig.ProxyProtocol.HTTP);
+        httpProxyRelayServerConfig.setRelayProtocol(HttpProxyServerConfig.ProxyProtocol.LEE);
+        // httpProxyRelayServerConfig.setRealProxyHost("74.91.26.90");
+        httpProxyRelayServerConfig.setRealProxyHost("8.210.18.42");
+        httpProxyRelayServerConfig.setRealProxyPort(40000);
+        // httpProxyRelayServerConfig.setProxyHost("sprint.ikeatw.com");
+        // httpProxyRelayServerConfig.setProxyPort(50000);
+        // httpProxyRelayServerConfig.setProxyUsername("sprint");
+        // httpProxyRelayServerConfig.setProxyPassword("FtMM7EvG");
+
+        httpProxyRelayServerConfig.setPort(40001);
+        httpProxyRelayServerConfig.setBossGroupThreads(5);
+        httpProxyRelayServerConfig.setWorkerGroupThreads(10);
+
+        ReplayRuleConfig replayRuleConfig = new ReplayRuleConfig();
+        replayRuleConfig.setDirectHosts(Arrays.asList("ip111", "baidu.com"));
+        httpProxyRelayServerConfig.setReplayFilterConfig(replayRuleConfig);
+        httpRelayProxyServer.start(httpProxyRelayServerConfig);
     }
 }
---
+++ proxy-client/src/main/java/io/le/proxy/client/gui/TrayGUI.java
@@ -0,0 +1,63 @@
+package io.le.proxy.client.gui;
+
+import lombok.extern.slf4j.Slf4j;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+@Slf4j
+public class TrayGUI {
+
+    private TrayIcon trayIcon;
+
+    public TrayGUI() {
+        if(! SystemTray.isSupported()) {
+            log.warn("The current system does not support trays!");
+            return;
+        }
+
+        try {
+            // 获得本操作系统托盘的实例
+            SystemTray tray = SystemTray.getSystemTray();
+
+            // 构造一个右键弹出式菜单
+            PopupMenu pop = new PopupMenu();
+            // MenuItem about = new MenuItem("关于我们");
+            // pop.add(about);
+            MenuItem exit = new MenuItem("Exit");
+            pop.add(exit);
+
+            // 显示在托盘中的图标
+            ImageIcon icon = new ImageIcon(getClass().getResource("/favicon.png"));
+            trayIcon = new TrayIcon(icon.getImage(), "Proxy", pop);
+            // 这句很重要，没有会导致图片显示不出来
+            trayIcon.setImageAutoSize(true);
+            // 监听鼠标
+            trayIcon.addMouseListener(new MouseAdapter() {
+                public void mouseClicked(MouseEvent e) {
+                    if (e.getClickCount() == 2) {
+                        // Double click
+                    }
+
+                }
+            });
+
+            exit.addActionListener(e -> {
+                if(e.getSource() == exit) {
+                    setToolTip("Exiting...");
+                    System.exit(0);
+                }
+            });
+            tray.add(trayIcon);
+            log.info("Create system tray successfully");
+        } catch (Exception e) {
+            log.error("Create system tray exception", e);
+        }
+    }
+
+    public void setToolTip(String tooltip) {
+        trayIcon.setToolTip(tooltip);
+    }
+}
