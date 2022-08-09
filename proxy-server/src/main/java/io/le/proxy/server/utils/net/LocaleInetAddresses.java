dfff6b8b1db6e6fec7491b30e6cfdb1b3d64a5e7
lile <285051173@qq.com>
2022-08-09 18:34:06 +0800

    multiIP

--- proxy-server/src/main/java/io/le/proxy/server/utils/net/LocaleInetAddresses.java
+++ proxy-server/src/main/java/io/le/proxy/server/utils/net/LocaleInetAddresses.java
@@ -5,6 +5,8 @@ import io.le.proxy.server.utils.lang.StringUtils;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.InputStream;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
@@ -19,19 +21,22 @@ public class LocaleInetAddresses {
     private static InetAddress[] inetAddresses;
 
     static {
-        try (InputStream inputStream = LocaleInetAddresses.class.getResourceAsStream("/ip_addresses.txt")) {
-            String ips = IOUtils.toString(inputStream, StandardCharsets.UTF_8);
-            inetAddresses = Arrays.stream(ips.split("\\s+")).map(String::trim).filter(StringUtils::isNotBlack).map(ip -> {
-                try {
-                    return InetAddress.getByName(ip);
-                } catch (UnknownHostException e) {
-                    log.error("ip=" + ip, e);
-                }
-                return null;
-            }).filter(Objects::nonNull).toArray(InetAddress[]::new);
-            log.debug("成功加载到IP列表: {}", Arrays.asList(inetAddresses));
-        } catch (Exception e) {
-            log.error("未读取到IP配置， 使用默认IP: {}", e.getMessage());
+        File file = new File("ip_addresses.txt");
+        if(file.exists()) {
+            try (InputStream inputStream = new FileInputStream(file)) {
+                String ips = IOUtils.toString(inputStream, StandardCharsets.UTF_8);
+                inetAddresses = Arrays.stream(ips.split("\\s+")).map(String::trim).filter(StringUtils::isNotBlack).map(ip -> {
+                    try {
+                        return InetAddress.getByName(ip);
+                    } catch (UnknownHostException e) {
+                        log.error("ip=" + ip, e);
+                    }
+                    return null;
+                }).filter(Objects::nonNull).toArray(InetAddress[]::new);
+                log.debug("Successfully loaded into IP list: {}", Arrays.asList(inetAddresses));
+            } catch (Exception e) {
+                log.error("IP configuration not read, use default IP: {}", e.getMessage());
+            }
         }
     }
 
