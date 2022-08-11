e099e897cc194d6918cb5fc73446b984c16b04bf
lile <285051173@qq.com>
2022-08-11 10:04:01 +0800

    HTTPS proxy protocol

--- curl.bat
+++ curl.bat
@@ -1,1 +1,5 @@
-curl --insecure -v -x https://127.0.0.1:40002 https://ipinfo.io
+# --insecure跳过https请求证书校验
+curl --insecure -v -x http://127.0.0.1:40002 https://ipinfo.io
+
+# --proxy-insecure 跳过https代理证书校验
+curl --proxy-insecure -v -x  https://127.0.0.1:40002 https://ipinfo.io -k
