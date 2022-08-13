a78dd51f3b0f197ed272779f8bf682993ae22541
lile <lile@LAPTOP-LILE>
2022-08-13 15:28:20 +0800

    socks5 relay debug

--- curl.bat
+++ curl.bat
@@ -5,8 +5,9 @@ curl --insecure -v -x http://127.0.0.1:40000 https://ipinfo.io
 curl --proxy-insecure -v -x  https://127.0.0.1:40000 https://ipinfo.io -k
 
 # socks5代理
-curl -v -x  socks5://127.0.0.1:40000 https://ipinfo.io
+curl -v -x  socks5://auh:123123@127.0.0.1:40000 https://ipinfo.io
 
 
 # 中继代理
+curl -v -x  http://auh:456789@127.0.0.1:40001 https://ipinfo.io -k
 curl -v -x  http://auh:456789@127.0.0.1:40001 https://ipinfo.io -k
